c53ccb94791905864402f1dfcc6531b8483afc23
akashygigmos <53926890+ray2199@users.noreply.github.com>
2024-06-18 22:22:21 +0530

    auth

--- routers/auth_routers.py
+++ routers/auth_routers.py
@@ -4,7 +4,7 @@ from typing import Annotated, Union
 from fastapi import APIRouter, Depends, FastAPI, HTTPException, Response, status, Cookie
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-
+from fastapi.responses import JSONResponse
 import auth
 import crud
 import schemas
@@ -12,11 +12,13 @@ from auth import oauth2_scheme
 from dependencies import get_db
 from models import User
 from schemas import UserCreate, UserInDB
+import requests
+from fastapi import FastAPI, Request, HTTPException, Header, Body
 
 app = APIRouter(prefix="/auth", tags=["auth"])
 
 
-@app.post("/token", response_model=schemas.Token)
+@app.post("/token")
 def login_for_access_token(
     response: Response,
     form_data: OAuth2PasswordRequestForm = Depends(),
@@ -35,6 +37,9 @@ def login_for_access_token(
     access_token = auth.create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
     )
+    response = JSONResponse(
+        status_code=200, content={"access_token": access_token, "token_type": "bearer"}
+    )
     response.set_cookie(
         key="access_token",
         value=f"Bearer {access_token}",
@@ -42,7 +47,7 @@ def login_for_access_token(
         samesite="none",  # Ensure this is set to 'none'
         secure=True,
     )
-    return {"access_token": access_token, "token_type": "bearer"}
+    return response
 
     # return user
 
@@ -68,21 +73,18 @@ def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 @app.get("/users/me", response_model=schemas.UserBase)
 def read_users_me(
     response: Response,
-    access_token: Union[str, None] = Cookie(None),
+    authorization: Annotated[Union[str, None], Header()] = None,
     db: Session = Depends(get_db),
 ):
-    print(access_token)
-    if access_token:
-        token = access_token.replace("Bearer ", "")
-        credentials_exception = HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-        token_data = auth.verify_token(token)
-        user = crud.get_user_by_email(db, email=token_data.username)
-        if user is None:
-            raise credentials_exception
-        return user
-    else:
-        return {"UserName": "NONE", "Email": "NONE", "Role": "NONE"}
+    token = auth(authorization)
+    token_data = auth.verify_token(token)
+    user = crud.get_user_by_email(db, email=token_data.username)
+    return user
+
+
+def auth(authorization):
+    if not authorization:
+        raise HTTPException(status_code=400, detail="Authorization Is Required")
+    # Getting token from the header
+    access_token = authorization.replace("Bearer ", "")
+    return access_token
